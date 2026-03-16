6efc2741ce1d796fd8bbdfc79599ff9b4a274b32
hond64 <wentzelmiguel@gmail.com>
2026-03-16 12:44:33 +0000

    implement heapify method for heapsort

--- tryheapsort.java
+++ tryheapsort.java
@@ -66,6 +66,13 @@ public class tryHeapsort{
             }
         }
     }
+    public static void heapify(String[] array, int n, int i){
+        int largest = i;    
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if(left < n && array[left].compareTo(array[largest]) > 0){
+            largest = left;
+            
 
         
 }   
